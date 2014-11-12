7d8fe0bada9367c0d87b8fa2d5134e3c3dc1fffb
Skyler Manzanares <skymanzanares@hotmail.com>
2014-11-11 23:49:42 -0700

    Made random items be generated for EVERY chest in the map. Spawns 1-3 items per chest

--- src/fr/skyost/hungergames/tasks/RandomItems.java
+++ src/fr/skyost/hungergames/tasks/RandomItems.java
@@ -24,29 +24,29 @@ public class RandomItems extends BukkitRunnable {
 	
 	@Override
 	public void run() {
-		final Location location = HungerGames.currentMap.getSpawnLocation();
-		location.add(random.nextInt(doubledRandomDistance) - HungerGames.config.gameRandomItemsDistance + 1, 0, random.nextInt(doubledRandomDistance) - HungerGames.config.gameRandomItemsDistance + 1);
-		final int y = HungerGames.currentMap.getHighestBlockYAt(location);
-		location.setY(y);
-		final ItemStack item = pickRandomItem();
-		if(item != null) {
-			if(HungerGames.config.gameRandomItemsChests) {
-				final Block chestBlock = location.getBlock();
-				chestBlock.setType(Material.CHEST);
-				final Chest chest = (Chest)chestBlock.getState();
-				chest.getInventory().addItem(item);
-				final Block under = location.clone().subtract(0, 1, 0).getBlock();
-				if(!under.getType().isSolid()) {
-					under.setType(Material.GRASS);
+//		final Location location = HungerGames.currentMap.getSpawnLocation();
+//		location.add(random.nextInt(doubledRandomDistance) - HungerGames.config.gameRandomItemsDistance + 1, 0, random.nextInt(doubledRandomDistance) - HungerGames.config.gameRandomItemsDistance + 1);
+//		final int y = HungerGames.currentMap.getHighestBlockYAt(location);
+//		location.setY(y);
+		ItemStack item;
+		int i, max;
+		if(HungerGames.config.gameRandomItemsChests) {
+//			final Block chestBlock = location.getBlock();
+//			chestBlock.setType(Material.CHEST);
+//			final Chest chest = (Chest)chestBlock.getState();
+//			chest.getInventory().addItem(item);
+//			final Block under = location.clone().subtract(0, 1, 0).getBlock();
+//			if(!under.getType().isSolid()) {
+//				under.setType(Material.GRASS);
+//			}
+			for (Chest chest : HungerGames.chestHolder.getChests()) {
+				max = random.nextInt(3) + 1;
+				for (i = 0; i < max; i++) {
+					item = pickRandomItem();
+					chest.getInventory().addItem(item);
 				}
 			}
-			else {
-				HungerGames.currentMap.dropItem(location, item);
-			}
-			HungerGamesAPI.broadcastMessage(HungerGames.messages.message10.replace("/x/", String.valueOf(location.getBlockX())).replace("/y/", String.valueOf(y)).replace("/z/", String.valueOf(location.getBlockZ())));
-			if(HungerGames.config.gameRandomItemsThundering) {
-				HungerGames.currentMap.strikeLightningEffect(location);
-			}
+			
 		}
 		else {
 			HungerGames.logsManager.log("Unable to spawn the specified item. One of your items is invalid.", Level.WARNING);
